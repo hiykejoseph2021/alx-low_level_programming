9e2b3821d4c0cbb027f2b584ca00aae920fc532b
Ayoola <ikjoseph33@gmail.com>
2023-02-16 15:11:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by newline.
+ * main - prints "Progrmming is like multilingual
+ * puzzle, followed by new line.
  * Return: Always 0.
  */
 
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
+
 	return (0);
 }
