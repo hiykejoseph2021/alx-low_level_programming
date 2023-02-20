e5df5eb9d4b527e77623d22f6dc6574e0a679f2d
Ayoola <ikjoseph33@gmail.com>
2023-02-20 14:16:33 -0800

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,30 @@
 
 int main(void)
 {
-	int c, i, k, j;
+	int c, i, k;
 
-	for (i = 48; i <= 57; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (k = 48; c <= 57; i++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			for (k = 48; <= 57; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
-				for (j = 48; <= 57; j++)
+				if (c < i && i < k)
 				{
-					if (((k = j) > (c + i) && k >= c) || c < k)
-					{
 						putchar(c);
 						putchar(i);
 						putchar(k);
-						putchar(j);
+
+						if (c != '7')
+						{
+							putchar(';');
+							putchar(' ');
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
 
