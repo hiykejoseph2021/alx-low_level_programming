cba7d1ef625e7849f575afe36c1406715766e168
Ayoola <ikjoseph33@gmail.com>
2023-02-20 13:11:58 -0800

    print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,15 +11,11 @@ int main(void)
 	char low;
 
 	for (low = 'a'; 'z'; low++)
-	{
-		putchar(low);
-	}
-	
-	{
-		for (low = 'A'; low <= 'Z'; low++);
-		putchar(low);
-	}
+	putchar(low);
 
+	for (low = 'A'; low <= 'Z'; low++);
+	putchar(low);
 	putchar('\n');
+	
 	return (0);
 }
